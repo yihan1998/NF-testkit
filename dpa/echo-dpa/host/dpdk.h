9fcaf54e6e21b9738d214270f531ec6eee957720
yihan <yangyihan1998@gmail.com>
2024-10-10 13:53:26 +0000

    add print

--- dpa/echo-dpa/host/dpdk.h
+++ dpa/echo-dpa/host/dpdk.h
@@ -1,6 +1,20 @@
 #ifndef _DPDK_H_
 #define _DPDK_H_
 
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_malloc.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_eal.h>
+#include <rte_launch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+
 int config_ports(void);
 int run_dpdk_loop(void);
 
