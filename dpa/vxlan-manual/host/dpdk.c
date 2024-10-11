3166851fab7295db9a3726156dedfeef9acaf569
yihan <yangyihan1998@gmail.com>
2024-10-11 09:35:43 +0000

    add vxlan manual

---
+++ dpa/vxlan-manual/host/dpdk.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include "dpdk.h"
+
+#define USEC_PER_SEC    1000000L
+#define TIMEVAL_TO_USEC(t)  ((t.tv_sec * USEC_PER_SEC) + t.tv_usec)
+
+#define DEFAULT_PKT_BURST   32
+#define DEFAULT_RX_DESC     4096
+#define DEFAULT_TX_DESC     4096
+
+int nb_cores;
+
+/* RX queue configuration */
+static struct rte_eth_rxconf rx_conf = {
+    .rx_thresh = {
+        .pthresh = 8,
+        .hthresh = 8,
+        .wthresh = 4,
+    },
+    .rx_free_thresh = 32,
+};
+
+/* TX queue configuration */
+static struct rte_eth_txconf tx_conf = {
+    .tx_thresh = {
+        .pthresh = 36,
+        .hthresh = 0,
+        .wthresh = 0,
+    },
+    .tx_free_thresh = 0,
+};
+
+/* Port configuration */
+struct rte_eth_conf port_conf = {
+    .rxmode = {
+        .mq_mode = RTE_ETH_MQ_RX_RSS,
+    },
+    .txmode = {
+        .mq_mode = RTE_ETH_MQ_TX_NONE,
+        .offloads = (RTE_ETH_TX_OFFLOAD_IPV4_CKSUM |
+                RTE_ETH_TX_OFFLOAD_UDP_CKSUM |
+                RTE_ETH_TX_OFFLOAD_TCP_CKSUM),
+    },
+    .rx_adv_conf = {
+        .rss_conf = {
+            .rss_key = NULL,
+            .rss_hf =
+                RTE_ETH_RSS_IP | RTE_ETH_RSS_TCP | RTE_ETH_RSS_UDP,
+        },
+    },
+};
+
+/* Packet mempool for each core */
+struct rte_mempool * pkt_mempools[8];
+
+int config_ports(void) {
+    int ret;
+    uint16_t portid;
+    char name[RTE_MEMZONE_NAMESIZE];
+    uint16_t nb_rxd = DEFAULT_RX_DESC;
+    uint16_t nb_txd = DEFAULT_TX_DESC;
+
+    nb_cores = rte_lcore_count();
+
+    for (int i = 0; i < nb_cores; i++) {
+        /* Create mbuf pool for each core */
+        sprintf(name, "mbuf_pool_%d", i);
+        pkt_mempools[i] = rte_pktmbuf_pool_create(name, 8192,
+            RTE_MEMPOOL_CACHE_MAX_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+            rte_socket_id());
+        if (pkt_mempools[i] == NULL) {
+            rte_exit(EXIT_FAILURE, "Cannot init mbuf pool\n");
+        } else {
+            printf("MBUF pool %u: %p...\n", i, pkt_mempools[i]);
+        }
+    }
+
+    /* Initialise each port */
+	RTE_ETH_FOREACH_DEV(portid) {
+		struct rte_eth_dev_info dev_info;
+
+        printf("Initializing port %u...", portid);
+		fflush(stdout);
+
+		ret = rte_eth_dev_info_get(portid, &dev_info);
+		if (ret != 0) {
+			rte_exit(EXIT_FAILURE, "Error during getting device (port %u) info: %s\n", portid, strerror(-ret));
+        }
+
+		/* Configure the number of queues for a port. */
+		ret = rte_eth_dev_configure(portid, nb_cores, nb_cores, &port_conf);
+		if (ret < 0) {
+			rte_exit(EXIT_FAILURE, "Cannot configure device: err=%d, port=%u\n", ret, portid);
+        }
+		/* >8 End of configuration of the number of queues for a port. */
+
+        for (int i = 0; i < nb_cores; i++) {
+            /* RX queue setup. 8< */
+            ret = rte_eth_rx_queue_setup(portid, i, nb_rxd, rte_eth_dev_socket_id(portid), &rx_conf, pkt_mempools[i]);
+            if (ret < 0) {
+                rte_exit(EXIT_FAILURE, "rte_eth_rx_queue_setup:err=%d, port=%u\n", ret, portid);
+            }
+
+            ret = rte_eth_tx_queue_setup(portid, i, nb_txd, rte_eth_dev_socket_id(portid), &tx_conf);
+            if (ret < 0) {
+                rte_exit(EXIT_FAILURE, "rte_eth_tx_queue_setup:err=%d, port=%u\n", ret, portid);
+            }
+        }
+		/* >8 End of queue setup. */
+
+		fflush(stdout);
+
+		/* Start device */
+		ret = rte_eth_dev_start(portid);
+		if (ret < 0) {
+			rte_exit(EXIT_FAILURE, "rte_eth_dev_start:err=%d, port=%u\n", ret, portid);
+        }
+
+		printf("done\n");
+        ret = rte_eth_promiscuous_enable(portid);
+        if (ret != 0) {
+            rte_exit(EXIT_FAILURE, "rte_eth_promiscuous_enable:err=%s, port=%u\n", rte_strerror(-ret), portid);
+        }
+    }
+
+    return 0;
+}
+
+int run_dpdk_loop(void) {
+    int portid;
+    struct rte_mbuf * rx_pkts[DEFAULT_PKT_BURST];
+    int nb_rx, nb_tx;
+	RTE_ETH_FOREACH_DEV(portid) {
+        for (int i = 0; i < nb_cores; i++) {
+            nb_rx = rte_eth_rx_burst(portid, i, rx_pkts, DEFAULT_PKT_BURST);
+            if (nb_rx) {
+                printf("Receive %d packets\n", nb_rx);
+                nb_tx = rte_eth_tx_burst(portid, i, rx_pkts, nb_rx);
+                printf("Send %d packets\n", nb_tx);
+                if (unlikely(nb_tx < nb_rx)) {
+                    do {
+                        rte_pktmbuf_free(rx_pkts[nb_tx]);
+                    } while (++nb_tx < nb_rx);
+                }
+            }
+        }
+    }
+    return 0;
+}
