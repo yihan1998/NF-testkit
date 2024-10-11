ca451b2f00b49a829671bd450a538d017a034eca
yihan <yangyihan1998@gmail.com>
2024-10-11 12:15:48 +0000

    test flexio window step by step

--- dpa/flow_monitor2/host/vxlan_host.c
+++ dpa/flow_monitor2/host/vxlan_host.c
@@ -785,6 +785,12 @@ int main(int argc, char ** argv)
         // flexio_msg_stream_flush(default_stream);
 		sleep(1);
 		run_dpdk_loop();
+		for (int i = 0; i < MAX_NB_THREAD; i++) {
+            printf("%d ", app_cfg.host_buffer[i]);
+            if (i % 16 == 15) {
+                printf("\n");
+            }
+        }
 	}
 
 	vxlan_destroy(&app_cfg);
