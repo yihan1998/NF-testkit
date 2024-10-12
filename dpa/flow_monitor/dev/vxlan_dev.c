cdd9b3922d379b411734f90850b9a6c1db2b18d7
yihan <yangyihan1998@gmail.com>
2024-10-12 05:02:50 +0000

    add print info

--- dpa/flow_monitor/dev/vxlan_dev.c
+++ dpa/flow_monitor/dev/vxlan_dev.c
@@ -250,17 +250,15 @@ void __dpa_global__ vxlan_device_event_handler(uint64_t index)
 	result = flexio_dev_window_config(dtctx, dev_ctx->window_id, dev_ctx->mkey);
 	if (result != FLEXIO_DEV_STATUS_SUCCESS) {
 		flexio_dev_print("Failed to configure FlexIO window\n");
-		return -1;
     }
 
     /* Acquire device pointer to host memory */
 	result = flexio_dev_window_ptr_acquire(dtctx, dev_ctx->haddr, (flexio_uintptr_t *)&dev_ctx->host_buffer);
 	if (result != FLEXIO_DEV_STATUS_SUCCESS) {
 		flexio_dev_print("Failed to acquire FlexIO window ptr\n");
-		return -1;
     }
 
-	flexio_dev_print("Init packet count %lu(%p)\n", *dev_ctx->host_buffer, dev_ctx->host_buffer);
+	flexio_dev_print("Init packet count %d(%p)\n", *dev_ctx->host_buffer, (void *)dev_ctx->host_buffer);
 
 	// struct flexio_dev_thread_ctx *dtctx;
     // struct device_context *dev_ctx = &dev_ctxs[index];
@@ -272,7 +270,7 @@ void __dpa_global__ vxlan_device_event_handler(uint64_t index)
 	 */
 	while (flexio_dev_cqe_get_owner(dev_ctx->rq_cq_ctx.cqe) != dev_ctx->rq_cq_ctx.cq_hw_owner_bit) {
 		/* Print the message */
-		flexio_dev_print("Process packet: %ld, seen packet: %lu(%p)\n", dev_ctx->packets_count++, *dev_ctx->host_buffer, dev_ctx->host_buffer);
+		flexio_dev_print("Process packet: %ld, seen packet: %d(%p)\n", dev_ctx->packets_count++, *dev_ctx->host_buffer, (void *)dev_ctx->host_buffer);
 		/* Update memory to DPA */
 		__dpa_thread_fence(__DPA_MEMORY, __DPA_R, __DPA_R);
 		/* Process the packet */
