7cd4a9a7f6ee214ef6853b7d140a6b519c214cec
yihan <yangyihan1998@gmail.com>
2024-10-28 06:49:58 +0000

    debugging ddos

--- dpa/ddos-manual/host/doca.c
+++ dpa/ddos-manual/host/doca.c
@@ -13,48 +13,66 @@ static doca_error_t create_match_pipe(struct doca_flow_port *port, int port_id, 
 	struct doca_flow_match match;
 	struct doca_flow_actions actions, *actions_arr[NB_ACTIONS_ARR];
 	struct doca_flow_fwd fwd;
+#ifdef ENABLE_COUNTER
 	struct doca_flow_monitor counter;
+#endif	/* ENABLE_COUNTER */
 	struct doca_flow_pipe_cfg *pipe_cfg;
 	doca_error_t result;
 
 	memset(&match, 0, sizeof(match));
 	memset(&actions, 0, sizeof(actions));
 	memset(&fwd, 0, sizeof(fwd));
+#ifdef ENABLE_COUNTER
 	memset(&counter, 0, sizeof(counter));
+#endif	/* ENABLE_COUNTER */
 
 	/* 5 tuple match */
-	SET_MAC_ADDR(match.outer.eth.dst_mac, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff);
+	match.parser_meta.outer_l3_type = DOCA_FLOW_L3_META_IPV4;
+	match.outer.l3_type = DOCA_FLOW_L3_TYPE_IP4;
+	// match.outer.ip4.src_ip = 0xffffffff;
+	match.outer.ip4.dst_ip = 0xffffffff;
+	// match.outer.l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_TRANSPORT;
+	// match.outer.transport.src_port = 0xffff;
+	// match.outer.transport.dst_port = 0xffff;
 
+	/* set meta data to match on the egress domain */
+	actions.meta.pkt_meta = UINT32_MAX;
+	actions.outer.l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_TRANSPORT;
+	actions.outer.transport.src_port = 0xffff;
 	actions_arr[0] = &actions;
 
+#ifdef ENABLE_COUNTER
 	counter.counter_type = DOCA_FLOW_RESOURCE_TYPE_NON_SHARED;
+#endif	/* ENABLE_COUNTER */
 
 	result = doca_flow_pipe_cfg_create(&pipe_cfg, port);
 	if (result != DOCA_SUCCESS) {
-		printf("Failed to create doca_flow_pipe_cfg: %s\n", doca_error_get_descr(result));
+		DOCA_LOG_ERR("Failed to create doca_flow_pipe_cfg: %s", doca_error_get_descr(result));
 		return result;
 	}
 
 	result = set_flow_pipe_cfg(pipe_cfg, "MATCH_PIPE", DOCA_FLOW_PIPE_BASIC, true);
 	if (result != DOCA_SUCCESS) {
-		printf("Failed to set doca_flow_pipe_cfg: %s\n", doca_error_get_descr(result));
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg: %s", doca_error_get_descr(result));
 		goto destroy_pipe_cfg;
 	}
 	result = doca_flow_pipe_cfg_set_match(pipe_cfg, &match, NULL);
 	if (result != DOCA_SUCCESS) {
-		printf("Failed to set doca_flow_pipe_cfg match: %s\n", doca_error_get_descr(result));
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg match: %s", doca_error_get_descr(result));
 		goto destroy_pipe_cfg;
 	}
 	result = doca_flow_pipe_cfg_set_actions(pipe_cfg, actions_arr, NULL, NULL, NB_ACTIONS_ARR);
 	if (result != DOCA_SUCCESS) {
-		printf("Failed to set doca_flow_pipe_cfg monitor: %s\n", doca_error_get_descr(result));
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg monitor: %s", doca_error_get_descr(result));
 		goto destroy_pipe_cfg;
 	}
+#ifdef ENABLE_COUNTER
 	result = doca_flow_pipe_cfg_set_monitor(pipe_cfg, &counter);
 	if (result != DOCA_SUCCESS) {
-		printf("Failed to set doca_flow_pipe_cfg counter: %s\n", doca_error_get_descr(result));
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg counter: %s", doca_error_get_descr(result));
 		goto destroy_pipe_cfg;
 	}
+#endif	/* ENABLE_COUNTER */
 
 	/* forwarding traffic to other port */
 	fwd.type = DOCA_FLOW_FWD_PORT;
@@ -66,25 +84,31 @@ destroy_pipe_cfg:
 	return result;
 }
 
-/*
- * Add DOCA Flow pipe entry with example 5 tuple match
- *
- * @pipe [in]: pipe of the entry
- * @status [in]: user context for adding entry
- * @return: DOCA_SUCCESS on success and DOCA_ERROR otherwise.
- */
-static doca_error_t add_match_pipe_entry(struct doca_flow_pipe *pipe, int id,
+static doca_error_t add_match_pipe_entry(struct doca_flow_pipe *pipe, 
 							struct entries_status *status, 
 						   	struct doca_flow_pipe_entry **entry)
 {
 	struct doca_flow_match match;
 	struct doca_flow_actions actions;
+	// struct doca_flow_pipe_entry *entry;
 	doca_error_t result;
+
+	doca_be32_t dst_ip_addr = BE_IPV4_ADDR(10, 10, 10, 10);
+	// doca_be32_t src_ip_addr = BE_IPV4_ADDR(1, 2, 3, 4);
+	// doca_be16_t dst_port = rte_cpu_to_be_16(80);
+	// doca_be16_t src_port = rte_cpu_to_be_16(1234);
 
 	memset(&match, 0, sizeof(match));
 	memset(&actions, 0, sizeof(actions));
 
-	SET_MAC_ADDR(match.outer.eth.dst_mac, 0xa0, 0x99, 0xc2, 0x31, 0xf7, id);
+	match.outer.ip4.dst_ip = dst_ip_addr;
+	// match.outer.ip4.src_ip = src_ip_addr;
+	// match.outer.transport.dst_port = dst_port;
+	// match.outer.transport.src_port = src_port;
+
+	actions.meta.pkt_meta = 1;
+	// actions.outer.transport.src_port = rte_cpu_to_be_16(1235);
+	actions.action_idx = 0;
 
 	result = doca_flow_pipe_add_entry(0, pipe, &match, &actions, NULL, NULL, 0, status, entry);
 	if (result != DOCA_SUCCESS)
