8e287ee050b6bec0dbde81315094e9ae22bdc749
yihan <yangyihan1998@gmail.com>
2024-10-28 06:45:25 +0000

    debugging ddos

--- dpa/ddos-manual/host/doca.c
+++ dpa/ddos-manual/host/doca.c
@@ -6,6 +6,91 @@
 
 #include "flow_common.h"
 
+static doca_error_t create_match_pipe(struct doca_flow_port *port, int port_id, struct doca_flow_pipe **pipe)
+{
+	struct doca_flow_match match;
+	struct doca_flow_actions actions, *actions_arr[NB_ACTIONS_ARR];
+	struct doca_flow_fwd fwd;
+	struct doca_flow_monitor counter;
+	struct doca_flow_pipe_cfg *pipe_cfg;
+	doca_error_t result;
+
+	memset(&match, 0, sizeof(match));
+	memset(&actions, 0, sizeof(actions));
+	memset(&fwd, 0, sizeof(fwd));
+	memset(&counter, 0, sizeof(counter));
+
+	/* 5 tuple match */
+	SET_MAC_ADDR(match.outer.eth.dst_mac, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff);
+
+	actions_arr[0] = &actions;
+
+	counter.counter_type = DOCA_FLOW_RESOURCE_TYPE_NON_SHARED;
+
+	result = doca_flow_pipe_cfg_create(&pipe_cfg, port);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to create doca_flow_pipe_cfg: %s", doca_error_get_descr(result));
+		return result;
+	}
+
+	result = set_flow_pipe_cfg(pipe_cfg, "MATCH_PIPE", DOCA_FLOW_PIPE_BASIC, true);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg: %s", doca_error_get_descr(result));
+		goto destroy_pipe_cfg;
+	}
+	result = doca_flow_pipe_cfg_set_match(pipe_cfg, &match, NULL);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg match: %s", doca_error_get_descr(result));
+		goto destroy_pipe_cfg;
+	}
+	result = doca_flow_pipe_cfg_set_actions(pipe_cfg, actions_arr, NULL, NULL, NB_ACTIONS_ARR);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg monitor: %s", doca_error_get_descr(result));
+		goto destroy_pipe_cfg;
+	}
+	result = doca_flow_pipe_cfg_set_monitor(pipe_cfg, &counter);
+	if (result != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Failed to set doca_flow_pipe_cfg counter: %s", doca_error_get_descr(result));
+		goto destroy_pipe_cfg;
+	}
+
+	/* forwarding traffic to other port */
+	fwd.type = DOCA_FLOW_FWD_PORT;
+	fwd.port_id = port_id ^ 1;
+
+	result = doca_flow_pipe_create(pipe_cfg, &fwd, NULL, pipe);
+destroy_pipe_cfg:
+	doca_flow_pipe_cfg_destroy(pipe_cfg);
+	return result;
+}
+
+/*
+ * Add DOCA Flow pipe entry with example 5 tuple match
+ *
+ * @pipe [in]: pipe of the entry
+ * @status [in]: user context for adding entry
+ * @return: DOCA_SUCCESS on success and DOCA_ERROR otherwise.
+ */
+static doca_error_t add_match_pipe_entry(struct doca_flow_pipe *pipe, int id,
+							struct entries_status *status, 
+						   	struct doca_flow_pipe_entry **entry)
+{
+	struct doca_flow_match match;
+	struct doca_flow_actions actions;
+	doca_error_t result;
+
+	memset(&match, 0, sizeof(match));
+	memset(&actions, 0, sizeof(actions));
+
+	SET_MAC_ADDR(match.outer.eth.dst_mac, 0xa0, 0x99, 0xc2, 0x31, 0xf7, id);
+
+	result = doca_flow_pipe_add_entry(0, pipe, &match, &actions, NULL, NULL, 0, status, entry);
+	if (result != DOCA_SUCCESS)
+		return result;
+
+	return DOCA_SUCCESS;
+}
+
 doca_error_t doca_init(int nb_queues)
 {
 	int nb_ports = 2;
@@ -20,8 +105,6 @@ doca_error_t doca_init(int nb_queues)
 	struct doca_flow_pipe *pipe;
 	struct entries_status status_ingress;
 	int num_of_entries_ingress = 1;
-	struct entries_status status_egress;
-	int num_of_entries_egress = 1;
 	doca_error_t result;
 	int port_id;
 
@@ -41,7 +124,6 @@ doca_error_t doca_init(int nb_queues)
 
 	for (port_id = 0; port_id < nb_ports; port_id++) {
 		memset(&status_ingress, 0, sizeof(status_ingress));
-		memset(&status_egress, 0, sizeof(status_egress));
 
 		result = create_match_pipe(ports[port_id], port_id, &pipe);
 		if (result != DOCA_SUCCESS) {
